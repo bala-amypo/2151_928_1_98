cb925e4b42478dfccdef38c4dfe460d5b8a0cb9f
bala-amypo <bala@amypo.in>
2025-12-23 04:37:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/MicroLessonRepository.java
+++
@@ -1,4 +0,0 @@
-public interface MicroLessonRepository extends JpaRepository<MicroLesson, Long> {
-    List<MicroLesson> findByTagsContainingAndDifficultyAndContentType(
-        String tags, String difficulty, String contentType);
-}
--- src/main/java/com/example/demo/repository/RecommendationRepository.java
+++
@@ -1,5 +0,0 @@
-public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
-    List<Recommendation> findByUserIdOrderByGeneratedAtDesc(Long userId);
-    List<Recommendation> findByUserIdAndGeneratedAtBetween(
-        Long userId, LocalDateTime start, LocalDateTime end);
-}
