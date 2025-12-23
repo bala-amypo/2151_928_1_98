0c461c469e99432c68e495b6cea0990b8086938f
bala-amypo <bala@amypo.in>
2025-12-23 04:04:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,18 @@
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest req) {
+        return userService.login(req.getEmail(), req.getPassword());
+    }
+}
