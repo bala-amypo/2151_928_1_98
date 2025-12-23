36334a8c9af5f4cce9029248cb655cc33c272af3
bala-amypo <bala@amypo.in>
2025-12-23 04:40:27 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++
@@ -1,9 +0,0 @@
-@Configuration
-public class SwaggerConfig {
-    @Bean
-    public OpenAPI api() {
-        return new OpenAPI().info(
-            new Info().title("Micro-Learning API")
-                      .version("1.0"));
-    }
-}
