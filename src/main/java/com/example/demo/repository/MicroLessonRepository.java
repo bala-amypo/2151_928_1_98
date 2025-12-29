869e48029d138d67d1f18e8257a375f3b39ddd42
bala-amypo <bala@amypo.in>
2025-12-29 08:51:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/MicroLessonRepository.java
+++ src/main/java/com/example/demo/repository/MicroLessonRepository.java
@@ -1,4 +1,4 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
 import com.example.demo.model.MicroLesson;
 import org.springframework.data.jpa.repository.JpaRepository;
